b78dc8454dd0d4c0ad0e615ae04e8d80012b3c7c
aurelienlair <aurelien.lair@onebip.com>
2016-11-24 23:06:26 +0100

    vim: adding into the compile script the possibility to copy/paste from guest/host VM

--- scripts/install-vim-7.4.sh
+++ scripts/install-vim-7.4.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-function installVim() {
-    sudo apt-get install -y lua5.1 liblua5.1-dev libncurses5-dev libgnome2-dev \
-        libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev \
-        libx11-dev libxpm-dev libxt-dev python-dev ruby-dev wget
-
-    mkdir -p /tmp/install
-    cd /tmp/install
-    rm -rf vim74
-    wget ftp://ftp.de.vim.org/unix/vim-7.4.tar.bz2
-    tar xvjf vim-7.4.tar.bz2
-    cd vim74
-
     ./configure --with-features=huge \
                 --enable-gui=gtk2 \
                 --enable-cscope \
@@ -19,7 +6,7 @@ function installVim() {
                 --with-lua-prefix=/usr \
                 --enable-pythoninterp \
                 --enable-rubyinterp
-
+    
     make VIMRUNTIMEDIR=/usr/share/vim/vim74
     sudo make install
 }
