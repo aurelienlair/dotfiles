77abd68d45aa7d505f32aa8dcdfe69af3999138e
Aurélien Lair <aurelien.lair@gmail.com>
2019-04-20 22:48:02 +0200

    dotfile: use last version of mps

--- scripts/install-mps.sh
+++ scripts/install-mps.sh
@@ -4,7 +4,11 @@ apt-get install vlc -y
 apt-get install python3-pip -y
 apt-get install python-setuptools -y
 apt-get install python-dev -y
+apt-get install python3-setuptools
 
+pip3 install --upgrade wheel
 pip3 install --upgrade setuptools
-pip3 install --upgrade --user mps-youtube
+git -C /tmp/ clone git@github.com:mps-youtube/mps-youtube.git
+cd /tmp/mps-youtube
+pip3 install --upgrade --user .
 pip3 install --upgrade --user youtube-dl
