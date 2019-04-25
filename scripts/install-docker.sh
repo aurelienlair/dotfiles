dc9c86edefd77f146b461b1d588a27a3a5afaf33
Aurélien Lair <aurelien.lair@gmail.com>
2019-04-25 18:52:13 +0200

    dotfiles: updating docker-compose version

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -8,7 +8,8 @@ sudo apt-get update
 sudo apt-get install docker-ce
 
 # https://docs.docker.com/compose/install/
-sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo rm -rf /usr/local/bin/docker-compose
+sudo curl -Lv https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # https://docs.docker.com/install/linux/linux-postinstall/
