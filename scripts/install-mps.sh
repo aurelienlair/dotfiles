4c2c45d21963a5f09b3277f296d942afba5ebed5
Aurélien Lair <aurelien.lair@gmail.com>
2019-06-22 12:01:46 +0200

    dotfiles: removing old vim version, adding proper uninstall on MPS, fix missing hash tag on XSV script

--- scripts/install-mps.sh
+++ scripts/install-mps.sh
@@ -6,9 +6,20 @@ apt-get install python-setuptools -y
 apt-get install python-dev -y
 apt-get install python3-setuptools
 
+
+pip3 uninstall wheel
+pip3 uninstall setuptools
+
 pip3 install --upgrade wheel
 pip3 install --upgrade setuptools
+
+su aurelien
+
+pip3 uninstall mpsyt
+pip3 uninstall youtube-dl
+
 git -C /tmp/ clone git@github.com:mps-youtube/mps-youtube.git
 cd /tmp/mps-youtube
+
 pip3 install --upgrade --user .
 pip3 install --upgrade --user youtube-dl
--- scripts/install-xsv.sh
+++ scripts/install-xsv.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 curl -sSf https://sh.rustup.rs | sh
 export PATH=$PATH:~/.cargo/bin
 echo "Remember to add this command to your bashrc: export PATH=$PATH:~/.cargo/bin"
