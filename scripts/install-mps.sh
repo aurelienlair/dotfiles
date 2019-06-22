83a4b2cb3786eb9dab9c840c186160fcb354af3d
Aurélien Lair <aurelien.lair@gmail.com>
2019-06-22 18:25:26 +0200

    dotfiles: fix uninstall command on MPS

--- scripts/install-mps.sh
+++ scripts/install-mps.sh
@@ -7,19 +7,19 @@ apt-get install python-dev -y
 apt-get install python3-setuptools
 
 
-pip3 uninstall wheel
-pip3 uninstall setuptools
+pip3 uninstall wheel --yes
+pip3 uninstall setuptools --yes
 
 pip3 install --upgrade wheel
 pip3 install --upgrade setuptools
 
-su aurelien
-
-pip3 uninstall mpsyt
-pip3 uninstall youtube-dl
+su - aurelien -c 'pip3 uninstall mpsyt --yes'
+su - aurelien -c 'pip3 uninstall youtube-dl --yes'
 
 git -C /tmp/ clone git@github.com:mps-youtube/mps-youtube.git
 cd /tmp/mps-youtube
 
-pip3 install --upgrade --user .
-pip3 install --upgrade --user youtube-dl
+su - aurelien -c 'pip3 install --upgrade --user /tmp/mps-youtube'
+su - aurelien -c 'pip3 install --upgrade --user youtube-dl'
+
+rm -rf /tmp/mps-youtube
