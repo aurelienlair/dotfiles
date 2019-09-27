3e975b4f892829ee06724bb69ea578366b84239c
Aurélien Lair <aurelien.lair@gmail.com>
2019-09-27 09:44:39 +0200

    dotfiles: improve docker install command

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # https://docs.docker.com/install/linux/docker-ce/ubuntu/
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get -y install docker-ce
 
 # https://docs.docker.com/compose/install/
 sudo rm -rf /usr/local/bin/docker-compose
